1c761bb947318aa46f247c13136c91b0d23ce4d3
seyha <sinseyha1x@gmail.com>
2022-05-02 14:06:02 +0700

    new

--- read_file.py
+++ read_file.py
@@ -1,8 +1,17 @@
 import os
 import pandas
 import glob
-import csv
+import csv 
+class MyTemplate:
+    def __init__(self,shop_name,brand,model,price,date):
+        self.shop_name = shop_name
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.date = date
+# list of directory name
 paths = ['./Shop A/','./Shop B/','./Shop C/']
+list = []
 outputFilePath = 'result.csv'
 for path in paths:
     print(path)
@@ -10,7 +19,15 @@ for path in paths:
         if file.endswith(".csv"):
             csv = pandas.read_csv(os.path.join(path ,file))
             print(csv)
-        if file.endswith(".xlsx"):
+        if file.endswith(".xlsx") or file.endswith(".xls"):
             worksheet = pandas.read_excel(os.path.join(path ,file), sheet_name=None)
             print(worksheet)
-            # print(file)
+# iterate list
+for i in list:
+    pass
+# function for read csv file
+def todo_read_csv():
+    pass
+# function for read excel file
+def todo_read_excel():
+    pass
