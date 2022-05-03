4c3d445abbd806b893fd7f5cb66def16124045c9
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-05-04 00:40:37 +0700

    com

--- read_file.py
+++ read_file.py
@@ -1,30 +1,24 @@
 import os
-from unittest import result
-from numpy import pad
 import pandas
-import glob
-import csv 
-class MyTemplate:
-    def __init__(self,shop_name,brand,model,price,date):
-        self.shop_name = shop_name
-        self.brand = brand
-        self.model = model
-        self.price = price
-        self.date = date
 # list of directory name
 paths = ['./Shop A/','./Shop B/','./Shop C/']
-list = []
 outputFilePath = 'result.csv'
+# remove existing content
+file = open(outputFilePath,'r+')
+file.truncate(0)
+file.close()
+# 
 for path in paths:
-    # print(path)
     for file in os.listdir(path):
         if file.endswith(".csv"):
-            csv = pandas.read_csv(os.path.join(path ,file))
+            df= pandas.read_csv(os.path.join(path ,file))
             # print(csv['Price'].str.isnumeric() == True)
-            csv.to_csv('result.csv', index=False)
+            df.to_csv(outputFilePath,mode='a')
         if file.endswith(".xlsx"):
             worksheet = pandas.read_excel(os.path.join(path ,file), sheet_name=None,usecols='D:G')
             df = pandas.DataFrame(worksheet.items())
-            print(df)
-            df.to_csv('result.csv',mode='a')
+            print(df.index)
+            # for index, row in df.iterrows():
+            #     print (row["Date"], row["Price"])
+            df.to_csv(outputFilePath, mode='a', index=True, header=True)
     
